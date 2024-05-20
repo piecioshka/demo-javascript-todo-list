0055d1aaa60b92d64ad4217115a549c4953076dc
Piotr Kowalski <piecioshka@gmail.com>
2024-05-20 18:09:03 +0200

    Fix removing items

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -2,66 +2,71 @@
  * @typedef {Object} TodoListProps
  * @property {HTMLInputElement} $input
  * @property {HTMLUListElement} $list
-*/
+ */
 
 const ENTER_KEY_CODE = 13;
 
 class TodoList {
-
+  /**
+   * @param {TodoListProps} options
+   */
+  constructor(options) {
+    this.list = [];
     /**
-     * @param {TodoListProps} options
+     * @type {TodoListProps}
      */
-    constructor(options) {
-        this.list = [];
-        /**
-         * @type {TodoListProps}
-         */
-        this.settings = Object.assign({
-            $input: null,
-            $list: null
-        }, options);
+    this.settings = Object.assign(
+      {
+        $input: null,
+        $list: null,
+      },
+      options
+    );
 
-        this._setupDOMEvents();
+    this._setupDOMEvents();
+  }
+
+  _setupDOMEvents() {
+    this.settings.$input.addEventListener("keydown", ({ keyCode }) => {
+      if (keyCode !== ENTER_KEY_CODE) {
+        return;
+      }
+
+      const value = this.settings.$input.value.trim();
+
+      if (value.length === 0) {
+        return;
+      }
+
+      this._addTask(value);
+      this._clearInput();
+    });
+  }
+
+  /**
+   * @param {string} value
+   */
+  _addTask(value) {
+    const task = new Task(value, {
+      onRemove: this._onRemove.bind(this),
+    });
+    // Update UI
+    this.settings.$list.appendChild(task.$el);
+    // Update memory
+    this.list.push(task);
+  }
+
+  /**
+   * @param {string} taskId
+   */
+  _onRemove(taskId) {
+    const index = this.list.findIndex((item) => item.id === taskId);
+    if (index !== -1) {
+      this.list.splice(index, 1);
     }
+  }
 
-    _setupDOMEvents() {
-        this.settings.$input
-            .addEventListener('keydown', ({ keyCode }) => {
-                if (keyCode !== ENTER_KEY_CODE) {
-                    return;
-                }
-
-                const value = this.settings.$input.value.trim();
-
-                if (value.length === 0) {
-                    return;
-                }
-
-                this._addTask(value);
-                this._clearInput();
-            });
-    }
-
-    /**
-     * @param {string} value
-     */
-    _addTask(value) {
-        const task = new Task(value, {
-          onRemove(taskId) {
-            const index = this.list.findIndex(item => item.id === taskId);
-            if (index !== -1) {
-              this.list.splice(index, 1);
-            }
-          }
-        });
-        // Update UI
-        this.settings.$list.appendChild(task.$el);
-        // Update memory
-        this.list.push(task);
-    }
-
-    _clearInput() {
-        this.settings.$input.value = '';
-    }
-
+  _clearInput() {
+    this.settings.$input.value = "";
+  }
 }
